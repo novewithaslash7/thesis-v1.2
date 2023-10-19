5bc08a0430bd868f3134d8808a429ea575d7d833
NOVE JHON PACHEJO <novejhon.pachejo@wvsu.edu.ph>
2023-10-19 11:16:14 +0800

    edited / october 19

--- quiz/index.php
+++ quiz/index.php
@@ -21,26 +21,21 @@
         <div class="w-1/2 p-5 bg-gray-200">
             <p class="text-center font-bold text-3xl mb-5">Instruction</p>
             <!-- do proper paragraph with BR's -->
-            <p>
-            During the Test:
-
-Read Instructions Carefully: Before beginning each section, read the instructions and questions carefully to understand the requirements.
-
-Manage Your Time: Pace yourself throughout the test to allocate enough time for each section or question. Don't get stuck on a single question; move on and return if necessary.
-
-Answer Each Question: Attempt to answer every question, even if you're unsure. You can revisit unanswered questions if time permits.
-
-Stay Calm and Focused: Maintain a calm and focused mindset. Take deep breaths if you start to feel anxious, and avoid rushing through the questions.
-
-Check Your Work: If time allows, review your answers for errors or omissions. Pay attention to detail.
+            <p>During the Test:</p>
+            
+<p>Read Instructions Carefully: Before beginning each section, read the instructions and questions carefully to understand the requirements.</p>
+<p>Manage Your Time: Pace yourself throughout the test to allocate enough time for each section or question. Don't get stuck on a single question; move on and return if necessary.</p>
+<p>Answer Each Question: Attempt to answer every question, even if you're unsure. You can revisit unanswered questions if time permits.</p>
+<p>Stay Calm and Focused: Maintain a calm and focused mindset. Take deep breaths if you start to feel anxious, and avoid rushing through the questions.</p>
+<p>Check Your Work: If time allows, review your answers for errors or omissions. Pay attention to detail.</p>
             </p>
             <div class="flex justify-center align-center gap-10 p-10">
+            <a href="../landing/">
+                    <button class="p-5 hover:bg-gray-400 border-black border-4 border-solid rounded rounded-full font-bold">Cancel</button>
+                </a>
                 <a href="./quiz.php">
                     <button class="p-5 hover:bg-gray-400 border-black border-4 border-solid rounded rounded-full font-bold">Proceed</button>
-                </a>
-                <a href="../landing/">
-                    <button class="p-5 hover:bg-gray-400 border-black border-4 border-solid rounded rounded-full font-bold">Cancel</button>
-                </a>                
+                </a>              
             </div>
         </div>
 
